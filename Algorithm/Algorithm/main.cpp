3af6a62339818812cc5651be2852bf19d1df34b7
ThimLohse <thim.lohse@hotmail.com>
2018-10-25 12:06:27 +0200

    header file fix. New compile info top of main

--- Algorithm/Algorithm/main.cpp
+++ Algorithm/Algorithm/main.cpp
@@ -6,80 +6,76 @@
 //  Copyright © 2018 Thim Lohse. All rights reserved.
 //
 
-//Standard headers
-#include <iostream>
-#include <string>
-#include <stdio.h>
+// Standard headers
 #include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <string>
 #include <vector>
 
-
-
-//Custom headers
-#include "func.cpp"
+// Custom headers
+#include "func.hpp"
 
 using namespace std;
 
 int main() {
 
+  // test command in terminal for c++11 features
+  // g++ -std=c++11 -stdlib=libc++ -Wno-c++98-compat main.cpp func.cpp func.hpp
+  // ./a.out < tsp.in
 
-    //test command in terminal for c++11 features
-    // xcrun clang++ -std=c++11 -stdlib=libc++ -Wno-c++98-compat -o proj1  main.cpp
-    // g++ -std=c++11 -stdlib=libc++ -Wno-c++98-compat -o proj1  main.cpp
-    // ./proj1 < tsp.in
+  //** The line we read from std **/
+  string line;
 
+  /** Checking if we are reading number of nodes or coordinate pair **/
+  bool first = true;
 
-    //** The line we read from std **/
-    string line;
+  //** Counter of number of nodes **/
+  int numNodes = 0;
 
-    /** Checking if we are reading number of nodes or coordinate pair **/
-    bool first = true;
+  //** Size class used to convert strings to numbers **/
+  string::size_type sz; // alias of size_t
 
-    //** Counter of number of nodes **/
-    int numNodes = 0;
+  //** Vector to hold coordinate pairs **/
+  vector<pair<double, double>> vertices;
 
-    //** Size class used to convert strings to numbers **/
-    string::size_type sz;     // alias of size_t
+  //** Vector to hold the calculated tour **/
+  vector<int> greedyTour;
 
-    //** Vector to hold coordinate pairs **/
-    vector<pair<double, double>> vertices;
+  //** Id of each node pair on input. Needed for graph construction later on **/
+  int id = 0;
 
-    //** Vector to hold the calculated tour **/
-    vector<int> greedyTour;
+  //** Initialize the random generator used for calculating random possible
+  // starting points for MinimizeGreedy **/
+  srand((unsigned int)(time(NULL)));
 
-    //** Id of each node pair on input. Needed for graph construction later on **/
-    int id = 0;
+  //** Read in all the vertices **/
+  while (getline(cin, line)) {
 
-    //** Initialize the random generator used for calculating random possible starting points for MinimizeGreedy **/
-    srand((unsigned int)(time(NULL)));
-
-    //** Read in all the vertices **/
-    while(getline(cin, line)){
-
-        //** Extract number of nodes **/
-        if(first){
-            numNodes = std::stod(line, &sz);
-            first = false;
-        }
-        //** Deconstruct each consecutive line into (x,y)-coordinates and store in vector
-        else{
-            double x = std::stod (line,&sz);
-            double y = std::stod (line.substr(sz));
-            vertices.push_back(make_pair(x, y));
-            id++;
-        }
+    //** Extract number of nodes **/
+    if (first) {
+      numNodes = std::stod(line, &sz);
+      first = false;
     }
-
-    //** Calculate greedy tour **/
-    greedyTour = greedy(vertices, 0);
-
-    for(int i : greedyTour){
-      cout << i << endl;
+    //** Deconstruct each consecutive line into (x,y)-coordinates and store in
+    // vector
+    else {
+      double x = std::stod(line, &sz);
+      double y = std::stod(line.substr(sz));
+      vertices.push_back(make_pair(x, y));
+      id++;
     }
+  }
 
-    //** Output length of greedy tour **/
-    //cout << tourLength(greedyTour, vertices) << endl;
+  //** Calculate greedy tour **/
+  greedyTour = Functions::greedy(vertices, 0);
 
+  for (int i : greedyTour) {
+    cout << i << endl;
+  }
 
-    return 0;
+  //** Output length of greedy tour **/
+  // cout << tourLength(greedyTour, vertices) << endl;
+
+  return 0;
 }
